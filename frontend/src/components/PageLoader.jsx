7861019fab53cc20d464371f03fb09744ce9433d
speedy <angelixflorez@gmail.com>
2026-07-07 12:58:15 -0500

    implement frontend core architecture, image optimization, and product management features

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,3 +1,11 @@
-export default function PageLoader() {
-  return <div>Loading...</div>;
-}
+import { LoaderIcon } from "lucide-react";
+
+const PageLoader = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <LoaderIcon className="size-10 animate-spin text-primary" />
+    </div>
+  );
+};
+
+export default PageLoader;
